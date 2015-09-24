690f756604ccda7f25d3f7e684f11c6d1c3f68e5
Nguyen Tam Phu (VN) <phunt@asiantech.vn>
2015-09-24 11:57:14 +0700

    del repo

--- app/views/users/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @user, :id, :name, :age, :created_at, :updated_at
--- config/unicorn.rb
+++
@@ -1,9 +0,0 @@
-root = "/home/ntp/capistrano/current"
-working_directory root
-pid "#{root}/tmp/pids/unicorn.pid"
-stderr_path "#{root}/log/unicorn.log"
-stdout_path "#{root}/log/unicorn.log"
-
-listen "/tmp/unicorn.capistrano.sock"
-worker_processes 1
-timeout 30
